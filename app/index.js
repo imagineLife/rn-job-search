d493ffb267dcfdb61ff0b260b8c23d33a6915584
Eric Laursen <ericjlaursen@gmail.com>
2023-09-01 13:26:59 -0400

    more files

--- app/index.js
+++ app/index.js
@@ -1,7 +1,22 @@
-import { View, Text } from 'react-native';
+import { View, ScrollView, SafeAreaView, Text } from 'react-native';
+import { useState } from 'react';
+import { Stack, useRouter } from 'expo-router';
+import { COLORS, icons, images, SIZES } from './../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from './../components';
+const Home = () => {
+  const router = useRouter();
 
-const Home = () => <View>
-  <Text>Hello React Native!</Text>
-</View>
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: {
+            backgroundColor: COLORS.lightWhite
+          }
+        }}
+      />
+    </SafeAreaView>
+  );
+};
 
-export default Home
+export default Home;
